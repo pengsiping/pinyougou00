118962dae3adca35d6073ac408ff8dba56985ae5
user <13340113608@163.com>
2019-07-26 11:13:14 +0800

    MQ-商品详细页服务-上下架页面生成/删除-- 运营商后台-用户活跃度分析

---
+++ pinyougou-parent/pinyougou-seckill-task/src/main/java/com/pinyougou/seckill/task/UserAnalyse.java
@@ -0,0 +1,43 @@
+package com.pinyougou.seckill.task;
+
+import com.pinyougou.mapper.TbAnalysePVMapper;
+import com.pinyougou.pojo.TbAnalysePV;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.support.atomic.RedisAtomicLong;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class UserAnalyse {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private TbAnalysePVMapper pvMapper;
+
+    @Scheduled(cron="0 0,30 * * * ? ")
+    public void UserCount(){
+        RedisAtomicLong count = new RedisAtomicLong("portalCount", redisTemplate.getConnectionFactory());
+        long num = count.get();
+        count.expire(0, TimeUnit.SECONDS);
+        Date date = new Date();
+        Example example = new Example(TbAnalysePV.class);
+        example.createCriteria().andIsNull("endTime");
+        List<TbAnalysePV> oldPVs = pvMapper.selectByExample(example);
+        if(oldPVs!=null&&oldPVs.size()!=0){
+            TbAnalysePV oldPV = oldPVs.get(0);
+            oldPV.setEndTime(date);
+            oldPV.setNum(num);
+            pvMapper.updateByPrimaryKey(oldPV);
+        }
+        TbAnalysePV newPV = new TbAnalysePV();
+        newPV.setStartTime(date);
+        pvMapper.insert(newPV);
+    }
+}
